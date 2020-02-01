aaadc56e61d9d4958ffa2e82c32ebf80d361a37f
AlexWhaley <alex.whaley@mediatonic.co.uk>
2020-02-01 13:54:01 +0000

    physics bike bits

---
+++ GangsterDan/Assets/Scripts/GrabberBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class GrabberBehaviour : MonoBehaviour
+{
+	[SerializeField]
+	private float _moveSpeed = 1f;
+	[SerializeField]
+	private float _grabTime = 1f;
+	private Transform _transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+		_transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void HandleInput()
+	{
+		Vector2 movementVector = Vector2.zero;
+
+		if (Input.GetKey(KeyCode.W))
+		{
+			movementVector.y = 1f;
+		}
+		else if (Input.GetKey(KeyCode.S))
+		{
+			movementVector.y = -1f;
+		}
+
+		if (Input.GetKey(KeyCode.D))
+		{
+			movementVector.x = 1f;
+		}
+		else if (Input.GetKey(KeyCode.A))
+		{
+			movementVector.x = -1f;
+		}
+	}
+}
