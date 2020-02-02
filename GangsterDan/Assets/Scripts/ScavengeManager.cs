67dc6ec0743dc340c39dd7f735ea7adefaed9db5
Dean Sinclair <deansinclair95@gmail.com>
2020-02-02 12:48:30 +0000

    Add logic for claw timer and game states

--- GangsterDan/Assets/Scripts/ScavengeManager.cs
+++ GangsterDan/Assets/Scripts/ScavengeManager.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScavengeManager : MonoBehaviour
 {
     public static ScavengeManager Instance;
 
-    public List<GameObject> SpawnableItems;
-    public Transform SpawnPoint;
-    public float SpawnDelay = 5;
+    public ScavengeState State;
     public float ConveyorSpeed = 2;
+    
+    [SerializeField] private List<GameObject> SpawnableItems;
+    [SerializeField] private Transform SpawnPoint;
+    [SerializeField] private float SpawnDelay = 5;
+    [SerializeField] private float TotalTime = 60.0f;
+    [SerializeField] private Text TimerText;
+    [SerializeField] private Text CountdownText;
 
     private List<ItemData> scavengedItems = new List<ItemData>();
-    private float time = 0;
+    private float spawnTime = 0;
+    private float timeRemaining;
 
     private void Awake()
     {
@@ -23,18 +30,58 @@ public class ScavengeManager : MonoBehaviour
         else
         {
             Instance = this;
-            time = SpawnDelay;
         }
+    }
+
+    public void Init()
+    {
+        spawnTime = SpawnDelay;
+        State = ScavengeState.Countdown;
+        timeRemaining = 3.99f;
+        CountdownText.text = "";
+        UpdateGameTimer();
     }
 
     private void Update()
     {
-        time += Time.deltaTime;
+        timeRemaining -= Time.deltaTime;
 
-        if (time >= SpawnDelay)
+        switch (State)
         {
-            SpawnItem();
-            time = 0;
+            case ScavengeState.Countdown:
+                UpdateCountdownTimer();
+
+                if (timeRemaining < 1.0f)
+                {
+                    State = ScavengeState.InGame;
+                    timeRemaining = TotalTime;
+                    CountdownText.text = "";
+                }
+                break;
+            case ScavengeState.InGame:
+                UpdateGameTimer();
+
+                spawnTime += Time.deltaTime;
+
+                if (spawnTime >= SpawnDelay)
+                {
+                    SpawnItem();
+                    spawnTime = 0;
+                }
+
+                if (timeRemaining < 0.0f)
+                {
+                    State = ScavengeState.TimeFinished;
+                    timeRemaining = 5.0f;
+                    TimerText.text = "00:00";
+                }
+                break;
+            case ScavengeState.TimeFinished:
+                if (timeRemaining < 0.0f)
+                {
+                    // Go to next screen.
+                }
+                break;
         }
     }
 
@@ -43,10 +90,28 @@ public class ScavengeManager : MonoBehaviour
         scavengedItems.Add(item);
     }
 
-    public void SpawnItem()
+    private void SpawnItem()
     {
         int i = Random.Range(0, SpawnableItems.Count);
 
         Instantiate(SpawnableItems[i], SpawnPoint.position, Quaternion.identity);
     }
+
+    private void UpdateGameTimer()
+    {
+        TimerText.text = string.Format("{0}:{1}", timeRemaining.ToString("D2"), (timeRemaining % 1).ToString());
+    }
+
+    private void UpdateCountdownTimer()
+    {
+        CountdownText.text = timeRemaining.ToString("D1");
+    }
+}
+
+
+public enum ScavengeState
+{
+    Countdown,
+    InGame,
+    TimeFinished
 }
